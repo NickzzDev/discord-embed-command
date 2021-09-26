3e71762e1ca676c8a01bf63145a57b3a92b41ec7
Nickzz <85542797+NickzzDev@users.noreply.github.com>
2021-09-26 17:56:58 +1000

    Create v12.js

---
+++ v12.js
@@ -0,0 +1,20 @@
+
+const Discord = require('discord.js')
+
+exports.run = async(client, message, args) => {
+    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You Do Not Have Permission To Use This Command!')
+    
+
+    const word = args.join(" ")
+
+if (!word) return message.channel.send(`:x: Missing a Message, Please Specify the Message!`)
+
+    message.delete()
+    const embed = new Discord.MessageEmbed()
+    .setColor('0x06F7C7')
+    .setDescription(`${word} \n\nCreated by [Nickzz](https://github.com/NickzzDev)`)
+    .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+    .setFooter(client.user.tag, client.user.displayAvatarURL({ dynamic: true }))
+    .setTimestamp()
+    message.channel.send(embed)
+}
